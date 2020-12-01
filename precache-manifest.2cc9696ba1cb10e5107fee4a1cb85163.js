a62d0563f0a4836b4a6be45ee83d86245e131d70
Cuong179 <nguyenvancuong1379@gmail.com>
2020-12-01 16:39:41 +0700

    Updates

---
+++ precache-manifest.2cc9696ba1cb10e5107fee4a1cb85163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41d268ad5984256cf4ba",
+    "url": "/TodoList/static/css/main.76a1ff64.chunk.css"
+  },
+  {
+    "revision": "41d268ad5984256cf4ba",
+    "url": "/TodoList/static/js/main.8e386ea2.chunk.js"
+  },
+  {
+    "revision": "23ef1ee30c4a1e2cab36",
+    "url": "/TodoList/static/js/runtime~main.90b1048d.js"
+  },
+  {
+    "revision": "e49b9e219d1e9d8cbcff",
+    "url": "/TodoList/static/css/2.f9fd68cf.chunk.css"
+  },
+  {
+    "revision": "e49b9e219d1e9d8cbcff",
+    "url": "/TodoList/static/js/2.910de2c4.chunk.js"
+  },
+  {
+    "revision": "819e55fe05fc516221286b93019e3dea",
+    "url": "/TodoList/index.html"
+  }
+];
